4556e65c18eb7882499e5b895851b5f3a8ab719a
danielfa11 <daniel.f.anderson1@outlook.com>
2025-06-19 11:27:38 -0400

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Daniel Anderson on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVMTests/MVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTests.swift
+//  MVVMTests
+//
+//  Created by Daniel Anderson on 6/19/25.
+//
+
+import Testing
+@testable import MVVM
+
+struct MVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
